4e2960476bd386caefa6e9a55559035dd1e18a2f
Jordon Bedwell <envygeeks@gmail.com>
2013-05-09 09:22:38 -0500

    Add Coveralls

--- spec/rspec/helper.rb
+++ spec/rspec/helper.rb
@@ -1,5 +1,35 @@
-require "simplecov"
-SimpleCov.start
+$:.unshift(File.expand_path("../../lib", __FILE__))
+ENV["RACK_ENV"] = "test"
+
+unless %W(no false).include?(ENV["COVERAGE"])
+  require "simplecov"
+  require "coveralls"
+
+  module Coveralls
+    NoiseBlacklist = [
+      "[Coveralls] Using SimpleCov's default settings.".green,
+      "[Coveralls] Set up the SimpleCov formatter.".green,
+      "[Coveralls] Outside the Travis environment, not sending data.".yellow
+    ]
+
+    def puts(message)
+      # Only prevent the useless noise on our terminals, not inside of the Travis or Circle CI.
+      unless NoiseBlacklist.include?(message) || ENV["TRAVIS"] == "true" || ENV["CI"] == "true"
+        $stdout.puts message
+      end
+    end
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+        Coveralls::SimpleCov::Formatter
+  ]
+
+  Coveralls.noisy = true
+  SimpleCov.start do
+    add_filter "/spec/"
+  end
+end
 
 MAILCHIMP_URL = %r!https://us6.api.mailchimp.com/1.3/\?method=(?:[a-z0-9]+)!
 EXPORT_URL = %r!https://us6.api.mailchimp.com/export/1.0/(?:[a-z0-9]+)!
